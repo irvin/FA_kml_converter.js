f94791ba1c39e904c36ff599ee166a67b0b14d62
Irvin <irvinfly@gmail.com>
2013-10-12 20:46:28 +0800

    Add option '-s' to separate output files.

--- fa_kml_converter.js
+++ fa_kml_converter.js
@@ -2,9 +2,16 @@ var fs = require('fs'),
     xml2js = require('xml2js'),
     handlebars = require("handlebars");
 
-
+// command arguments
 var filename = process.argv[2];
 var filename_unext = filename.split('.')[0];
+var arguments = process.argv[3];
+
+
+var opt_seg = false;    // Seperate each 150 node into single file.
+if (arguments){
+    opt_seg = (arguments == '-s')? true: false;
+};
 
 var xml = fs.readFileSync(filename, 'utf-8');
 
@@ -58,13 +65,34 @@ for (i=0; i<num; i++){
     if (lon > data.maxlon) data.maxlon = lon;
 };
 
-var buffer = template(data);
-//console.log(buffer);
 
-fs.writeFile(filename_unext + '.gpx', buffer, function(err) {
-    if (err) {
+// Seperate segment output
+var coor_all = data.coor.slice(0, data.coor.length);
+var totalnum = coor_all.length - 1;
+var begin = 0;
+var end = (opt_seg == true && totalnum > 150)? 150 : totalnum;
+var i = (opt_seg)? 1 : null;
+
+console.log('node numbers:', totalnum);
+
+while (end <= totalnum) {
+//    console.log('data_range', begin, end);
+    data.coor = coor_all.slice(begin, end);
+    
+    var buffer = template(data);
+    var serial = (i)? '_' + i : '';
+    var fn = filename_unext + serial + '.gpx';
+    
+    var err = fs.writeFileSync(fn, buffer);
+    if (err)
         console.log(err);
-    } else {
-        console.log("file saved as " + filename_unext + '.gpx');
-    }
-});
+    else
+        console.log("file saved as " + fn);
+    
+    i += 1;
+    if (end == totalnum) break;
+
+    begin = end + 1;
+    end += 150;
+    if (end > totalnum) end = totalnum;
+};
